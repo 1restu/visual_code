ce557db833ccede888e433e6a22a25cabd8d9dcf
restu <140035020+1restu@users.noreply.github.com>
2023-11-10 09:24:39 +0700

    Add files via upload

---
+++ PboRestu-Xi rpl1/matematika/src/script-utama.py
@@ -0,0 +1,13 @@
+#import
+from luas import lingkaran, persegi, segitiga
+
+from volume import bola, kerucut, kubik
+
+#panggil fungsi
+print(luas_lingkaran(21))
+print(luas_persegi(10))
+print(luas_segitiga(4,8))
+
+print(volume_bola(15))
+print(volume_kerucut(6,12))
+print(volume_kubik(12,13,16))
---
+++ PboRestu-Xi rpl1/matematika/src/volume/kerucut.py
@@ -0,0 +1,2 @@
+def volume_kerucut(r,t):
+    return 1/3 * (r*r) * t
